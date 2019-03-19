28f66f2b23397ceb54083e93014566b04a070bf1
Fajrendajs <40194093+Fajrendajs@users.noreply.github.com>
2019-03-19 13:43:49 +0100

    dodan todo datum

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input, List, Icon } from 'antd';
+import { Input, List, Icon, DatePicker } from 'antd';
 
 // Don't forget to include the CSS styles for antd!
 import 'antd/dist/antd.css';
@@ -17,7 +17,9 @@ export default class ToDo extends Component {
   handlePressEnter = e => {
     const todo = {
       index: this.state.todos.length,
-      content: e.target.value
+      content: e.target.value,
+      date: null,
+      dateString: ''
     };
 
     const newTodos = this.state.todos.concat(todo);
@@ -28,9 +30,36 @@ export default class ToDo extends Component {
     });
   };
 
+  setDate = (index, date, dateString) => {
+    let newTodos = [...this.state.todos];
+    newTodos[index].date = date;
+    newTodos[index].dateString = dateString;
+
+    this.setState({
+      todos: newTodos
+    });
+  };
+
   handleOnChange = e => {
     this.setState({
       todoItem: e.target.value
+    });
+  };
+
+  removeTodo = index => {
+    let newTodos = [...this.state.todos];
+
+    // Remove element
+    newTodos.splice(index, 1);
+
+    // Decrement greater indexes
+    for (let i = index; i < newTodos.length; i++) {
+      newTodos[i].index -= 1;
+    }
+
+    // Update state
+    this.setState({
+      todos: newTodos
     });
   };
 
@@ -49,7 +78,11 @@ export default class ToDo extends Component {
           locale={{ emptyText: 'No todo items' }}
           dataSource={this.state.todos}
           renderItem={item => (
-            <TodoItem todo={item} removeTodo={this.removeTodo} />
+            <TodoItem
+              todo={item}
+              removeTodo={this.removeTodo}
+              setDate={this.setDate}
+            />
           )}
         />
       </div>
@@ -59,13 +92,22 @@ export default class ToDo extends Component {
 
 class TodoItem extends Component {
   remove = e => {
-    this.props.removeTodo(this.todo.index);
+    this.props.removeTodo(this.props.todo.index);
+  };
+
+  handleDateChange = (date, dateString) => {
+    this.props.setDate(this.props.todo.index, date, dateString);
   };
 
   render() {
     return (
       <List.Item
         actions={[
+          <DatePicker
+            format="DD/MM/YYYY"
+            onChange={this.handleDateChange}
+            value={this.props.todo.date}
+          />,
           <Icon type="close-circle" theme="filled" onClick={this.remove} />
         ]}
       >
